ff6f9292680d0918422f699e09f166670204dc89
Ravencer <ravencer880@gmail.com>
2021-02-27 00:05:00 +0700

    Выполнил восемнадцатое обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
+/* eslint-disable prefer-const */
+window.addEventListener('DOMContentLoaded', () => {
 
+	function countTimer(deadline, interval) {
+		let timerHours = document.querySelector('#timer-hours'),
+			timerMinutes = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
+
+		function getTimeRemaining() {
+			let dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining  / 60 / 60);
+			return {
+				hours,
+				minutes,
+				seconds
+			};
+		}
+		function updateClock() {
+			let timer = getTimeRemaining();
+			if (timer.seconds >= 0) {
+				timerHours.textContent = ('0' + timer.hours).slice(-2);
+				timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+				timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+			} else {
+				timerHours.textContent = '00';
+				timerHours.style.color = 'red';
+				timerMinutes.textContent = '00';
+				timerSeconds.style.color = 'red';
+				timerSeconds.textContent = '00';
+				timerMinutes.style.color = 'red';
+				clearInterval(interval);
+			}
+		}
+		updateClock();
+	}
+	setInterval(countTimer, 1000, '28 february 2021', this);
+});
