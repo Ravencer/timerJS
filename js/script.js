e00c0ae56c3091baa0c3e156bb2040677ee327d3
Ravencer <ravencer880@gmail.com>
2021-02-27 03:01:14 +0700

    Выполнил девятнадцатое задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-const */
 window.addEventListener('DOMContentLoaded', () => {
 
+	//Таймер
 	function countTimer(deadline, interval) {
 		let timerHours = document.querySelector('#timer-hours'),
 			timerMinutes = document.querySelector('#timer-minutes'),
@@ -39,4 +40,68 @@ window.addEventListener('DOMContentLoaded', () => {
 		updateClock();
 	}
 	setInterval(countTimer, 1000, '28 february 2021', this);
+
+	//Меню
+	const toggleMenu = () => {
+		const btnMenu = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			closeBtn = document.querySelector('.close-btn'),
+			menuItems = menu.querySelectorAll('ul>li'),
+			menuLinks = menu.querySelectorAll('ul>li>a'),
+			btnMain = document.querySelector('main>a');
+
+		const handlerMenu = () => {
+			menu.classList.toggle('active-menu');
+		};
+		btnMenu.addEventListener('click', handlerMenu);
+		closeBtn.addEventListener('click', (handlerMenu));
+		menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+		menuLinks.forEach(elem => elem.addEventListener('click', e => {
+			e.preventDefault();
+			document.querySelector(elem.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		}));
+		console.log(btnMain);
+		btnMain.addEventListener('click', e => {
+			e.preventDefault();
+			document.querySelector(btnMain.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	};
+	toggleMenu();
+
+
+	//popup
+	const togglePopUp = () => {
+		const popup = document.querySelector('.popup'),
+			popupBtn = document.querySelectorAll('.popup-btn'),
+			popupClose = document.querySelector('.popup-close');
+		popupBtn.forEach(elem => elem.addEventListener('click', () => {
+			let count = 0;
+			popup.style.display = 'block';
+			if (window.screen.width > 768) {
+				popup.style.opacity = '0';
+				let intervalId = setInterval(() => {
+					if (count < 1) {
+						count += 0.1;
+						popup.style.opacity = count;
+					} else {
+						clearInterval(intervalId);
+						return;
+					}
+				}, 30);
+			}
+		}));
+		popupClose.addEventListener('click', () => {
+			popup.style.display = 'none';
+			if (window.screen > 768) {
+				popup.style.opacity = '0';
+			}
+		});
+	};
+	togglePopUp();
+
+
 });
